1b71cf2aa4f12fbcac072609120f1bb1d631ec89
Sovichet-Rathanak <rathanaksovichet@gmail.com>
2025-04-24 18:58:29 +0700

    testing other api

--- laravel/tests/Feature/CategoryTest.php
+++ laravel/tests/Feature/CategoryTest.php
@@ -2,19 +2,50 @@
 
 namespace Tests\Feature;
 
+use App\Models\Category;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    //testing if we can post category
+    public function test_posting_category(): void
     {
-        $response = $this->get('/api/categories');
+        $response = $this->postJson('/api/categories', ['name' => 'Soda']);
+        $response->assertStatus(201)->assertJsonFragment(['name' => $response['name']]);
 
-        $response->assertStatus(200);
+        $response = $this->postJson('/api/categories', ['name' => 'Snack']);
+        $response->assertStatus(201)->assertJsonFragment(['name' => $response['name']]);
     }
+
+    //testing if we can get all category
+    public function test_getting_all_category(): void
+    {
+       $response = $this ->get('/api/categories');
+       $response->assertStatus(200)->assertJsonCount(Category::count());
+    }
+
+    //test if we can get category by id
+    public function test_gettin_category_by_id():void
+    {
+        $response = $this->get('/api/categories/1');
+        $response->assertStatus(200)->assertJson(['id'=>$response['id']]);
+    }
+
+    //test if we can update category by id
+    public function test_updating_category_by_id():void
+    {
+        $response = $this->patch('/api/categories/2', ['name' => "Vegetable"]);
+        $response->assertStatus(200)->assertJson(['id'=> $response['id'], 'name' => $response['name']]);
+    }
+
+    //test if we can delete category by id
+    public function test_delete_category_by_id():void
+    {
+        $response = $this->delete('/api/categories/2');
+        $response->assertStatus(200)->assertJson(['id'=>$response['id']]);
+
+        $this->get('/api/categories/2')->assertStatus(200)->assertDontSee(['id'=>$response['id']]);
+    }  
 }
--- laravel/tests/Feature/ProductTest.php
+++ laravel/tests/Feature/ProductTest.php
@@ -2,19 +2,49 @@
 
 namespace Tests\Feature;
 
+use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ProductTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/api/products');
+    // testing to see if we can post product
+    public function test_posting_product(): void{
+        $response = $this->postJson('/api/products', ['name' => 'Cola', 'category_id' => '1', "pricing" => 1.00]);
+        $response->assertStatus(201)->assertJson(['name' => $response['name'], 'category_id' => $response['category_id'], "pricing" => $response['pricing']]);
 
-        $response->assertStatus(200);
+        $response = $this->postJson('/api/products', ['name' => 'Cheetos', 'category_id' => '2', "pricing" => 2.00]);
+        $response->assertStatus(201)->assertJson(['name' => $response['name'], 'category_id' => $response['category_id'], "pricing" => $response['pricing']]);
     }
+
+    //testing if we can get all product
+    public function test_getting_all_product(): void
+    {
+       $response = $this ->get('/api/products');
+       $response->assertStatus(200)->assertJsonCount(Product::count());
+    }
+
+    //test if we can get product by id
+    public function test_gettin_product_by_id():void
+    {
+        $response = $this->get('/api/products/1');
+        $response->assertStatus(200)->assertJson(['id'=>$response['id']]);
+    }
+
+    //test if we can update product by id
+    public function test_updating_product_by_id():void
+    {
+        $response = $this->patch('/api/products/2', ['name' => "Carrot"]);
+        $response->assertStatus(200)->assertJson(['id'=> $response['id'], 'name' => $response['name'], 'category_id' => $response['category_id'], "pricing" => $response['pricing']]);
+    }
+
+    //test if we can delete product by id
+    public function test_delete_product_by_id():void
+    {
+        $response = $this->delete('/api/products/2');
+        $response->assertStatus(200)->assertJson(['id'=>$response['id']]);
+
+        $this->get('/api/products/2')->assertStatus(200)->assertDontSee(['id'=>$response['id']]);
+    } 
 }
